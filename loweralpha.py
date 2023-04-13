cbca610d9ded79b4835ed13603543b3c264366b6
Enoch Atuahene <eatuahene@springfieldcollege.edu>
2023-04-13 02:18:44 -0400

    mess

--- loweralpha.py
+++ loweralpha.py
@@ -1,7 +1,15 @@
+"""
 def lower_alpha(arr): #make list of only alpha
     a = [i.lower() for i in arr] #list each char in string as lower case
     for i in a :
         if i.isalpha() == False:
             a.remove(i)
     return a
+"""
 
+
+def lower_alpha(self, arr): #make list of only alpha
+        a = [i.lower() for i in arr] #list each char in string as lower case
+        b = [j for j in a if j.isalpha() == True]
+        
+        return b
