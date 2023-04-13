60a9671e0b85fca6fe028c58a8aadc439b0ffd0b
Enoch Atuahene <eatuahene@springfieldcollege.edu>
2023-04-13 01:57:00 -0400

    Py file for reversing array

---
+++ reverse.py
@@ -0,0 +1,17 @@
+def reverse_list(arr): #reverse a list using two pointers
+    left = 0
+    right = len(arr)-1
+
+    while left <= right:
+        arr[left], arr[right] = arr[right], arr[left]
+        left += 1
+        right -= 1
+
+    return arr
+
+
+    
+
+    
+
+
