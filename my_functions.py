2cf004324f5633e74914344666391e5c8814af75
Enoch Atuahene <eatuahene@springfieldcollege.edu>
2023-04-13 01:58:11 -0400

    functions file added

---
+++ my_functions.py
@@ -0,0 +1,19 @@
+def reverse_list(arr): #reverse a list using two pointers
+    left = 0
+    right = len(arr)-1
+
+    while left <= right:
+        arr[left], arr[right] = arr[right], arr[left]
+        left += 1
+        right -= 1
+
+    return arr
+
+
+def lower_alpha(arr): #make list of only alpha
+    a = [i.lower() for i in arr] #list each char in string as lower case
+    for i in a :
+        if i.isalpha() == False:
+            a.remove(i)
+    return a
+
