97468a3edff4383b6fa87bcef807f5164bd43663
Enoch Atuahene <eatuahene@springfieldcollege.edu>
2024-09-07 19:33:19 -0500

    commented

--- loweralpha.py
+++ loweralpha.py
@@ -9,7 +9,7 @@ def lower_alpha(arr): #make list of only alpha
 
 
 def lower_alpha(self, arr): #make list of only alpha
-        a = [i.lower() for i in arr] #list each char in string as lower case
+        a = [i.lower() for i in arr] #list each char in string as lower case using list comprehension
         b = [j for j in a if j.isalpha() == True]
         
         return b
